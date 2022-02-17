120b25955904c3bbe016ae432833fd46e831473b
Abhishek1099 <79436348+Abhishek1099@users.noreply.github.com>
2022-02-18 03:11:40 +0530

    Update orderAPI.java

--- Assignments/Spring Rest/Assignment1/springREST7/src/main/java/com/Abhishek/springREST7/API/orderAPI.java
+++ Assignments/Spring Rest/Assignment1/springREST7/src/main/java/com/Abhishek/springREST7/API/orderAPI.java
@@ -1,4 +1,4 @@
-package com.madhu.springREST7.API;
+package com.Abhishek.springREST7.API;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
