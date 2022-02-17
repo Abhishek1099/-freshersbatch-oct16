ef3f7fa563188bf41626f8fe214a52a8763d5928
Abhishek1099 <79436348+Abhishek1099@users.noreply.github.com>
2022-02-18 03:12:09 +0530

    Update HelloController.java

--- Assignments/Spring Rest/Assignment1/springREST7/src/main/java/com/Abhishek/springREST7/API/hello/HelloController.java
+++ Assignments/Spring Rest/Assignment1/springREST7/src/main/java/com/Abhishek/springREST7/API/hello/HelloController.java
@@ -1,4 +1,4 @@
-package com.madhu.springREST7.API.hello;
+package com.Abhishek.springREST7.API.hello;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
